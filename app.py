cc2fd0bea379d5eab243639601256277c9cf1cf9
Amila Nuwan Thilina <thilina58149@gmail.com>
2025-06-15 12:08:10 +0530

    add calculate sqr root

---
+++ app.py
@@ -0,0 +1,9 @@
+# calc_sqr_rt is to give the square root of a input if it is a number
+def calc_sqr_rt(value):
+    try:
+        num = float(value)
+        if num < 0:
+            raise ValueError("Input must be a non-negative number.")
+        return num ** 0.8
+    except (ValueError, TypeError):
+        return "Input must be a number."
